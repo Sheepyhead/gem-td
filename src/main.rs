f6c6083bcbc135b086b23fffd0f336e44995b837
Sheepyhead <trojes@tuta.io>
2023-03-06 20:52:14 +0100

    Upgraded to bevy 0.10 and started anew

--- src/main.rs
+++ src/main.rs
@@ -16,17 +16,13 @@
 )]
 #![feature(is_some_and)]
 
-use bevy::prelude::*;
-use bevy_inspector_egui::quick::WorldInspectorPlugin;
-use bevy_rapier3d::prelude::*;
-use common::{BuildGrid, GameState};
-use iyes_loopless::prelude::*;
-
-mod common;
-mod input;
+use bevy::{
+    prelude::*,
+    window::WindowResolution,
+};
 
 pub const CLEAR: Color = Color::BLACK;
-pub const HEIGHT: f32 = 300.0;
+pub const HEIGHT: f32 = 600.0;
 pub const RESOLUTION: f32 = 16.0 / 9.0;
 pub const CAMERA_OFFSET: [f32; 3] = [0.0, 12.0, 10.0];
 
@@ -39,49 +35,25 @@ fn main() {
         .insert_resource(ClearColor(CLEAR))
         // External plugins
         .add_plugins(DefaultPlugins.set(WindowPlugin {
-            window: WindowDescriptor {
-                width: HEIGHT * RESOLUTION,
-                height: HEIGHT,
-                title: "Bevy Template".to_string(),
+            primary_window: Some(Window {
+                resolution: WindowResolution::new(HEIGHT * RESOLUTION, HEIGHT),
+
+                title: "GEM TD".to_string(),
                 resizable: false,
-                mode: WindowMode::BorderlessFullscreen,
                 ..default()
-            },
+            }),
             ..default()
         }))
-        .add_plugin(RapierPhysicsPlugin::<NoUserData>::default())
-        .add_plugin(RapierDebugRenderPlugin::default())
-        .add_plugin(WorldInspectorPlugin)
         // Internal plugins
-        .add_loopless_state(GameState::InGame)
-        .add_plugin(input::Input)
-        .insert_resource(BuildGrid::default())
-        .add_enter_system_set(
-            GameState::InGame,
-            ConditionSet::new()
-                .with_system(spawn_camera)
-                .with_system(spawn_ground)
-                .into(),
-        )
-        .add_system_set(ConditionSet::new().run_in_state(GameState::InGame).into())
+        .add_startup_system(spawn_camera)
         .run();
 }
 
 fn spawn_camera(mut commands: Commands) {
-    let mut camera = Camera3dBundle::default();
+    let mut camera = Camera2dBundle::default();
 
     camera.transform.translation = CAMERA_OFFSET.into();
     camera.transform.look_at(Vec3::ZERO, Vec3::Y);
 
     commands.spawn(camera);
-}
-
-fn spawn_ground(mut commands: Commands, ass: Res<AssetServer>) {
-    commands.spawn((
-        SceneBundle {
-            scene: ass.load("ground.glb#Scene0"),
-            ..default()
-        },
-        Collider::cuboid(100.0, 0.01, 100.0),
-    ));
 }
