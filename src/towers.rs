28e3e378850c8e5d0b19fe7b6adcc62efb813d5a
Sheepyhead <trojes@tuta.io>
2023-03-17 23:23:20 +0100

    Implemented gem types and qualities

--- src/towers.rs
+++ src/towers.rs
@@ -1,6 +1,9 @@
-use std::{fmt::Debug, time::Duration};
+use std::{fmt::Debug, ops::RangeInclusive, time::Duration};
 
-use bevy::{prelude::*, utils::HashSet};
+use bevy::{
+    prelude::{shape::Cube, *},
+    utils::HashSet,
+};
 use bevy_prototype_debug_lines::DebugLines;
 use seldom_map_nav::prelude::*;
 
@@ -44,6 +47,7 @@ impl Debug for BuildGrid {
 pub fn uncover_dirt(
     mut commands: Commands,
     mut mats: ResMut<Assets<StandardMaterial>>,
+    mut meshes: ResMut<Assets<Mesh>>,
     dirt: Query<Entity, With<Dirt>>,
 ) {
     for entity in &dirt {
@@ -56,11 +60,13 @@ pub fn uncover_dirt(
         let mut timer = Timer::from_seconds(time, TimerMode::Once);
         timer.tick(Duration::from_secs_f32(time));
 
-        let typ = GemType::random();
-
-        let gem_tower = GemTower { typ };
+        let gem_tower = GemTower {
+            typ: GemType::random(),
+            quality: GemQuality::random(),
+        };
         commands.entity(entity).insert((
-            mats.add(typ.into()),
+            meshes.add(Into::<Cube>::into(gem_tower).into()),
+            mats.add(gem_tower.typ.into()),
             gem_tower,
             Tower,
             LaserAttack::from(gem_tower),
@@ -104,6 +110,27 @@ pub enum GemType {
     Topaz,
 }
 
+#[derive(Clone, Copy)]
+pub enum GemQuality {
+    Chipped,
+    Flawed,
+    Normal,
+    Flawless,
+    Perfect,
+}
+
+impl GemQuality {
+    pub fn random() -> Self {
+        match fastrand::u8(0..5) {
+            0 => Self::Chipped,
+            1 => Self::Flawed,
+            2 => Self::Normal,
+            3 => Self::Flawless,
+            4 => Self::Perfect,
+            _ => panic!("Gem quality over 4 detected, this should not happen"),
+        }
+    }
+}
 impl From<GemType> for StandardMaterial {
     fn from(val: GemType) -> Self {
         let mut color: StandardMaterial = Into::<Color>::into(val).into();
@@ -146,27 +173,103 @@ impl GemType {
 #[derive(Component, Clone, Copy)]
 pub struct GemTower {
     typ: GemType,
+    quality: GemQuality,
 }
 
 impl From<GemTower> for LaserAttack {
+    #[allow(clippy::match_same_arms)]
     fn from(value: GemTower) -> Self {
         Self {
-            range: match value.typ {
-                GemType::Ruby
-                | GemType::Emerald
-                | GemType::Diamond
-                | GemType::Amethyst
-                | GemType::Opal => 5.,
-                GemType::Sapphire | GemType::Topaz => 6.,
-                GemType::Aquamarine => 4.,
+            range: match (value.typ, value.quality) {
+                (GemType::Emerald, GemQuality::Chipped) => 5.,
+                (GemType::Emerald, GemQuality::Flawed) => 5.5,
+                (GemType::Emerald, GemQuality::Normal) => 6.,
+                (GemType::Emerald, GemQuality::Flawless) => 7.,
+                (GemType::Emerald, GemQuality::Perfect) => 7.,
+                (GemType::Ruby, _) => 8.,
+                (GemType::Sapphire, GemQuality::Chipped) => 5.5,
+                (GemType::Sapphire, GemQuality::Flawed) => 7.5,
+                (GemType::Sapphire, GemQuality::Normal) => 8.,
+                (GemType::Sapphire, GemQuality::Flawless) => 8.5,
+                (GemType::Sapphire, GemQuality::Perfect) => 14.,
+                (GemType::Diamond, GemQuality::Chipped) => 5.,
+                (GemType::Diamond, GemQuality::Flawed) => 5.5,
+                (GemType::Diamond, GemQuality::Normal) => 6.,
+                (GemType::Diamond, GemQuality::Flawless) => 6.5,
+                (GemType::Diamond, GemQuality::Perfect) => 7.5,
+                (GemType::Amethyst, GemQuality::Chipped) => 10.,
+                (GemType::Amethyst, GemQuality::Flawed) => 12.,
+                (GemType::Amethyst, GemQuality::Normal) => 13.,
+                (GemType::Amethyst, GemQuality::Flawless) => 13.5,
+                (GemType::Amethyst, GemQuality::Perfect) => 16.,
+                (GemType::Opal, GemQuality::Chipped) => 6.,
+                (GemType::Opal, GemQuality::Flawed) => 7.,
+                (GemType::Opal, GemQuality::Normal) => 8.,
+                (GemType::Opal, GemQuality::Flawless) => 9.,
+                (GemType::Opal, GemQuality::Perfect) => 10.,
+                (GemType::Aquamarine, GemQuality::Chipped) => 3.5,
+                (GemType::Aquamarine, GemQuality::Flawed) => 3.65,
+                (GemType::Aquamarine, GemQuality::Normal) => 3.8,
+                (GemType::Aquamarine, GemQuality::Flawless) => 4.,
+                (GemType::Aquamarine, GemQuality::Perfect) => 5.5,
+                (GemType::Topaz, _) => 5.,
             },
             color: value.typ.into(),
-            damage: match value.typ {
-                GemType::Ruby => 4,
-                GemType::Sapphire | GemType::Emerald | GemType::Amethyst | GemType::Aquamarine => 2,
-                GemType::Diamond => 5,
-                GemType::Opal => 1,
-                GemType::Topaz => 3,
+            damage: match (value.typ, value.quality) {
+                (GemType::Emerald, GemQuality::Chipped) => Damage::Range(4..=7),
+                (GemType::Emerald, GemQuality::Flawed) => Damage::Range(10..=13),
+                (GemType::Emerald, GemQuality::Normal) => Damage::Range(15..=25),
+                (GemType::Emerald, GemQuality::Flawless) => Damage::Range(30..=37),
+                (GemType::Emerald, GemQuality::Perfect) => Damage::Range(80..=95),
+                (GemType::Ruby, GemQuality::Chipped) => Damage::Range(8..=9),
+                (GemType::Ruby, GemQuality::Flawed) => Damage::Range(13..=16),
+                (GemType::Ruby, GemQuality::Normal) => Damage::Range(20..=25),
+                (GemType::Ruby, GemQuality::Flawless) => Damage::Range(38..=45),
+                (GemType::Ruby, GemQuality::Perfect) => Damage::Range(80..=100),
+                (GemType::Sapphire, GemQuality::Chipped) => Damage::Range(5..=8),
+                (GemType::Sapphire, GemQuality::Flawed) => Damage::Range(10..=14),
+                (GemType::Sapphire, GemQuality::Normal) => Damage::Range(16..=22),
+                (GemType::Sapphire, GemQuality::Flawless) => Damage::Range(30..=40),
+                (GemType::Sapphire, GemQuality::Perfect) => Damage::Range(60..=80),
+                (GemType::Diamond, GemQuality::Chipped) => Damage::Range(8..=12),
+                (GemType::Diamond, GemQuality::Flawed) => Damage::Range(16..=18),
+                (GemType::Diamond, GemQuality::Normal) => Damage::Range(30..=37),
+                (GemType::Diamond, GemQuality::Flawless) => Damage::Range(58..=65),
+                (GemType::Diamond, GemQuality::Perfect) => Damage::Range(140..=150),
+                (GemType::Amethyst, GemQuality::Chipped) => Damage::Range(10..=15),
+                (GemType::Amethyst, GemQuality::Flawed) => Damage::Range(20..=27),
+                (GemType::Amethyst, GemQuality::Normal) => Damage::Range(30..=45),
+                (GemType::Amethyst, GemQuality::Flawless) => Damage::Range(60..=80),
+                (GemType::Amethyst, GemQuality::Perfect) => Damage::Range(140..=170),
+                (GemType::Opal, GemQuality::Chipped) => Damage::Fixed(5),
+                (GemType::Opal, GemQuality::Flawed) => Damage::Fixed(10),
+                (GemType::Opal, GemQuality::Normal) => Damage::Fixed(20),
+                (GemType::Opal, GemQuality::Flawless) => Damage::Fixed(40),
+                (GemType::Opal, GemQuality::Perfect) => Damage::Fixed(85),
+                (GemType::Aquamarine, GemQuality::Chipped) => Damage::Range(6..=8),
+                (GemType::Aquamarine, GemQuality::Flawed) => Damage::Range(12..=15),
+                (GemType::Aquamarine, GemQuality::Normal) => Damage::Range(24..=30),
+                (GemType::Aquamarine, GemQuality::Flawless) => Damage::Range(48..=55),
+                (GemType::Aquamarine, GemQuality::Perfect) => Damage::Range(100..=120),
+                (GemType::Topaz, GemQuality::Chipped) => Damage::Fixed(4),
+                (GemType::Topaz, GemQuality::Flawed) => Damage::Fixed(8),
+                (GemType::Topaz, GemQuality::Normal) => Damage::Fixed(14),
+                (GemType::Topaz, GemQuality::Flawless) => Damage::Fixed(25),
+                (GemType::Topaz, GemQuality::Perfect) => Damage::Fixed(75),
+            },
+        }
+    }
+}
+
+impl From<GemTower> for shape::Cube {
+    fn from(value: GemTower) -> Self {
+        shape::Cube {
+            size: match value.quality {
+                GemQuality::Chipped => 0.4,
+                GemQuality::Flawed => 0.8,
+                GemQuality::Normal => 1.2,
+                GemQuality::Flawless => 1.6,
+                GemQuality::Perfect => 2.0,
             },
         }
     }
@@ -176,7 +279,7 @@ impl From<GemTower> for LaserAttack {
 pub struct LaserAttack {
     range: f32,
     color: Color,
-    damage: u32,
+    damage: Damage,
 }
 
 impl LaserAttack {
@@ -207,7 +310,7 @@ impl LaserAttack {
 
                         writer.send(Damaged {
                             target: target_entity,
-                            value: attack.damage,
+                            value: attack.damage.clone().get_value(),
                         });
                     } else {
                         // Target is dead
@@ -223,7 +326,7 @@ impl LaserAttack {
                     );
 
                     if let Some((creep, distance)) = closest {
-                        if dbg!(attack.range.powf(2.)) >= dbg!(distance) {
+                        if attack.range.powf(2.) >= distance {
                             **target = Some(creep);
                         }
                     }
@@ -246,5 +349,20 @@ impl LaserAttack {
             }
         }
         closest
+    }
+}
+
+#[derive(Clone)]
+pub enum Damage {
+    Range(RangeInclusive<u32>),
+    Fixed(u32),
+}
+
+impl Damage {
+    pub fn get_value(self) -> u32 {
+        match self {
+            Damage::Range(range) => fastrand::u32(range),
+            Damage::Fixed(val) => val,
+        }
     }
 }
