cf6930e285b6308161737d661446eb4630b68bf1
Sheepyhead <trojes@tuta.io>
2023-03-30 12:14:41 +0200

    Added basic button

--- src/gui.rs
+++ src/gui.rs
@@ -13,15 +13,19 @@ pub struct Sidebar;
 
 impl Sidebar {
     pub fn spawn(mut commands: Commands, ass: Res<AssetServer>) {
-        let sidebar = commands
-            .spawn(NodeBundle {
-                style: Style {
-                    flex_direction: FlexDirection::ColumnReverse,
-                    size: Size::all(Val::Percent(100.)),
+        let full_screen = commands
+            .spawn((
+                NodeBundle {
+                    style: Style {
+                        position_type: PositionType::Absolute,
+                        flex_direction: FlexDirection::ColumnReverse,
+                        size: Size::all(Val::Percent(100.)),
+                        ..default()
+                    },
                     ..default()
                 },
-                ..default()
-            })
+                Sidebar,
+            ))
             .id();
 
         let sidebar_background = commands
@@ -33,9 +37,9 @@ impl Sidebar {
                     justify_content: JustifyContent::SpaceBetween,
                     padding: UiRect {
                         top: Val::Percent(1.),
+                        bottom: Val::Percent(1.),
                         left: Val::Percent(5.),
                         right: Val::Percent(5.),
-                        ..default()
                     },
                     size: Size::height(Val::Percent(100.)),
                     align_self: AlignSelf::End,
@@ -59,7 +63,38 @@ impl Sidebar {
             })
             .id();
 
-        commands.entity(sidebar).add_child(sidebar_background);
-        commands.entity(sidebar_background).add_child(title);
+        let button = commands
+            .spawn(ButtonBundle {
+                style: Style {
+                    size: Size::all(Val::Px(50.)),
+                    ..default()
+                },
+                background_color: Color::PINK.into(),
+                ..default()
+            })
+            .id();
+        let icon = commands
+            .spawn(ImageBundle {
+                image: UiImage::new(ass.load("chipped.PNG")),
+                style: Style {
+                    size: Size::width(Val::Percent(100.)),
+                    ..default()
+                },
+                ..default()
+            })
+            .id();
+
+        commands.entity(full_screen).add_child(sidebar_background);
+        commands
+            .entity(sidebar_background)
+            .add_child(title)
+            .add_child(button);
+        commands.entity(button).add_child(icon);
+    }
+
+    fn _despawn(mut commands: Commands, sidebar: Query<Entity, With<Sidebar>>) {
+        for sidebar in &sidebar {
+            commands.entity(sidebar).despawn_recursive();
+        }
     }
 }
